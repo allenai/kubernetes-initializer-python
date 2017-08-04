e29f3f1fe3bc8ca398d958fb7c37b3e73a60770b
Jesse Kinkead <jesse.kinkead@gmail.com>
2017-08-04 11:03:22 -0700

    Add configuration and instructions for PyPi publishing. (#4)

---
+++ PyPiReadme.rst
@@ -0,0 +1,9 @@
+.. This RST document is used to populate the PyPi project's description.
+
+=============================
+Python Kubernetes Initializer
+=============================
+
+This is a simple library for writing `Kubernetes initializers <https://kubernetes.io/docs/admin/extensible-admission-controllers/>`_.
+
+Please see `the project's github page <https://github.com/allenai/kubernetes-initializer-python>`_ for full details.
---
+++ setup.py
@@ -0,0 +1,38 @@
+# Adapted from https://github.com/pypa/sampleproject/blob/master/setup.py .
+
+from setuptools import setup, find_packages
+from codecs import open
+import os
+
+dirname = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file.
+with open(os.path.join(dirname, 'PyPiReadme.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ai2-kubernetes-initializer',
+    version='0.1.0a1',
+    description='A simple Kubernetes initializer library.',
+    long_description=long_description,
+    url='https://github.com/allenai/kubernetes-initializer-python',
+    author='Jesse Kinkead',
+    author_email='jesse.kinkead@gmail.com',
+    license='Apache License 2.0',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Systems Administration',
+        'License :: OSI Approved :: Apache Software License',
+
+        # TODO(jkinkead): Figure out which Python 3.x version we really depend on. We can probably
+        # run on 3.5 or 3.4.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='kubernetes initializer',
+    packages=find_packages(exclude=['test']),
+    install_requires=['kubernetes', 'requests[security]', 'urllib3'])
