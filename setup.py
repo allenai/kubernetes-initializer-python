c3010d1804bfe95029945cfad113d77a65b7a083
Jesse Kinkead <jessek@allenai.org>
2017-09-05 13:32:05 -0700

    Set status to Beta.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
         'Topic :: System :: Systems Administration',
